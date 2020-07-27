bc4479c9175358b3cd8044ae5792cd45cc5b7a67
CodeacAlice <alice.carry@lilo.org>
2020-07-27 14:10:58 +0200

    [feature] class Crayon

---
+++ assets/js/class.js
@@ -0,0 +1,46 @@
+class Crayon {
+    constructor (x, y, angle, idcan, color) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.idcan = idcan;
+        this.color = color
+    }
+
+    moveTo(x, y) {
+        this.x = x; this.y = y;
+        document.getElementById(this.idcan).getContext("2d").moveTo(x,y);
+    }
+
+    traceTo(x,y) {
+        this.x = x; this.y = y;
+        var ctx = document.getElementById(this.idcan).getContext("2d");
+        ctx.lineTo(x,y);
+        ctxS.stroke();
+    }
+
+    turn(degre) {
+        this.angle += degre * Math.PI / 180.0;
+    }
+
+    moveLine(long) {
+        var x1 = this.x + long * Math.cos(this.angle),
+            y1 = this.y + long * Math.sin(this.angle);
+        this.moveTo(x1,y1);
+    }
+    traceLine(long) {
+        var x1 = this.x + long * Math.cos(this.angle),
+            y1 = this.y + long * Math.sin(this.angle);
+        this.traceTo(x1,y1);
+    }
+
+
+    reinit(orX, orY) {
+        var c = document.getElementById(this.idcan), ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height); 
+        ctx.beginPath(); 
+        ctx.strokeStyle = this.color;
+        this.moveTo(orX, orY);
+        this.angle = 0;
+    }
+}
