d524202d5732e2e37332801563afc7c98e894bc4
Alice Carry <43727761+CodeacAlice@users.noreply.github.com>
2024-06-05 19:52:15 +0200

    Refactoring code flocon

--- assets/js/class.js
+++ assets/js/class.js
@@ -1,46 +1,76 @@
+
 class Crayon {
-    constructor (x, y, angle, idcan, color) {
-        this.x = x;
-        this.y = y;
-        this.angle = angle;
-        this.idcan = idcan;
-        this.color = color
+    /**
+     * @param {string} idCanvas 
+     * @param {string} color 
+     */
+    constructor (idCanvas, color) {
+        this.x = 0;
+        this.y = 0;
+        this.angle = 0;
+        this.idCanvas = idCanvas;
+        this.color = color;
+        this.ctx = document.getElementById(this.idCanvas).getContext("2d");
     }
 
-    moveTo(x, y) {
+    /**
+     * Déplace le crayon au point de coordonnées (x,y), sans tracer de ligne
+     * @param {number} x 
+     * @param {number} y 
+     */
+    deplaceAuPoint(x, y) {
         this.x = x; this.y = y;
-        document.getElementById(this.idcan).getContext("2d").moveTo(x,y);
+        this.ctx.moveTo(x,y);
     }
 
-    traceTo(x,y) {
+    /**
+     * Trace une ligne jusqu'au point de coordonnées (x,y)
+     * @param {number} x 
+     * @param {number} y 
+     */
+    traceAuPoint(x,y) {
         this.x = x; this.y = y;
-        var ctx = document.getElementById(this.idcan).getContext("2d");
-        ctx.lineTo(x,y);
-        ctx.stroke();
+        this.ctx.lineTo(x,y);
+        this.ctx.stroke();
     }
 
-    turn(degre) {
+    /**
+     * Tourne le crayon
+     * @param {number} degre 
+     */
+    tourne(degre) {
         this.angle += degre * Math.PI / 180.0;
     }
 
-    moveLine(long) {
-        var x1 = this.x + long * Math.cos(this.angle),
-            y1 = this.y + long * Math.sin(this.angle);
-        this.moveTo(x1,y1);
+    /**
+     * Déplace le crayon dans la direction dans laquelle il est tourné
+     * @param {number} distance Distance que parcourt le crayon
+     */
+    deplaceEnLigne(distance) {
+        let x1 = this.x + distance * Math.cos(this.angle),
+            y1 = this.y + distance * Math.sin(this.angle);
+        this.deplaceAuPoint(x1,y1);
     }
-    traceLine(long) {
-        var x1 = this.x + long * Math.cos(this.angle),
-            y1 = this.y + long * Math.sin(this.angle);
-        this.traceTo(x1,y1);
+    /**
+     * Déplace le crayon dans la direction dans laquelle il est tourné en traçant une ligne
+     * @param {number} longueur Longueur de la ligne tracée
+     */
+    traceEnLigne(longueur) {
+        let x1 = this.x + longueur * Math.cos(this.angle),
+            y1 = this.y + longueur * Math.sin(this.angle);
+        this.traceAuPoint(x1,y1);
     }
 
-
-    reinit(orX, orY) {
-        var c = document.getElementById(this.idcan), ctx = c.getContext("2d");
-        ctx.clearRect(0, 0, c.width, c.height); 
-        ctx.beginPath(); 
-        ctx.strokeStyle = this.color;
-        this.moveTo(orX, orY);
+    /**
+     * Réinitialise le canvas
+     * @param {number} origineX 
+     * @param {number} origineY 
+     */
+    reinitialiseLeCanvas(origineX, origineY) {
+        this.ctx.clearRect(0, 0, c.width, c.height); 
+        this.ctx.beginPath(); 
+        this.ctx.strokeStyle = this.color;
+        this.deplaceAuPoint(origineX, origineY);
         this.angle = 0;
     }
 }
